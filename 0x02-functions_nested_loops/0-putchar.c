51c0a0c1cdaccdf607f04f8820d9998cef8f2e44
sadiqui <abdellahsdiqui@gmail.com>
2022-09-13 08:24:15 -0700

    new project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch = 0;
+	while (ch <= 8)
+	{
+		_putchar(str[ch]);
+		ch++;
+	}
+	_putchar('\n');
+	return (0);
+}
