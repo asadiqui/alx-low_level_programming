3e61535e69996cc66aad9d4a166742d18d4e9fa6
sadiqui <abdellahsdiqui@gmail.com>
2022-09-13 08:46:32 -0700

    Make the code Betty compliant

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,17 +2,21 @@
 
 /**
  * main - prints _putchar, followed by a new line
+ *
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
 	char str[] = "_putchar";
 	int ch = 0;
+
 	while (ch <= 8)
 	{
 		_putchar(str[ch]);
 		ch++;
 	}
 	_putchar('\n');
+
 	return (0);
 }
