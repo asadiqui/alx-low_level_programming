0777ff33fd2b1ba11b49d74729763882f9127945
sadiqui <abdellahsdiqui@gmail.com>
2022-09-30 13:33:34 -0700

    New project

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create a script that creates a static library
+# called liball.a from all the .c files
+# that are in the current directory
+
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
